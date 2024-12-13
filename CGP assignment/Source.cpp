b6c95750131660d8efcc7c990fad312d57bae357
tako <chongzm-wm22@student.tarc.edu.my>
2024-12-14 04:17:54 +0800

    cheast

--- CGP assignment/Source.cpp
+++ CGP assignment/Source.cpp
@@ -49,14 +49,17 @@ struct Vec3 {
 		return a.x * b.x + a.y * b.y + a.z * b.z;
 	}
 };
-Vec3 cameraPosition = { 0.0f, 0.0f, 8.0f }; // Camera starting position
+Vec3 cameraPosition = { 0.0f, 0.0f, 10.0f }; // Camera starting position
 Vec3 target = { 0.0f, 0.0f, 0.0f };         // Point the camera looks at
 Vec3 upVector = { 0.0f, 1.0f, 0.0f };       // Up direction
-float radius = 8.0f;              // Distance from camera to target
+float radius = 10.0f;              // Distance from camera to target
 float yaw = 0.0f;                 // Horizontal angle (in radians)
 float pitch = 0.0f;               // Vertical angle (in radians)
 GLenum style_glu= GLU_LINE,style_gl=GL_LINE_LOOP;
 int style_switch;
+float tx, ty, tz,angle;
+float Oner=-10, Ofar=10, Pner=0.1, Pfar=100;
+bool ortho=false;
 GLUquadricObj* obj = NULL;
 LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -73,24 +76,34 @@ LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam
 			switch (style_switch)
 			{
 			case 0:
-				style_glu = GLU_POINT;
-				style_gl = GL_POINTS;
-				break;
-			case 1:
 				style_glu = GLU_LINE;
 				style_gl = GL_LINE_LOOP;
+				
 				break;
-			case 2:
+			case 1:
 				style_glu = GLU_FILL;
 				style_gl = GL_POLYGON;
 				break;
+			case 2:
+				
+				style_glu = GLU_POINT;
+				style_gl = GL_POINTS;
+				break;
 			default:
-				style_glu = GLU_LINE;
-				style_gl = GL_LINE_LOOP;
+				style_glu = GLU_FILL;
+				style_gl = GL_POLYGON;
 				break;
 			}
 			style_switch++;
 		}
+		if (wParam == 'O') { ortho = !ortho; }
+		if (wParam == 'Q') { angle += 5; }
+		if (wParam == 'E') { angle -= 5; }
+		if (wParam == 'W') { ty += speed; }
+		if (wParam == 'A') { tx -= speed; }
+		if (wParam == 'S') { ty -= speed; }
+		if (wParam == 'D') { tx += speed; }
+
 		if (wParam == VK_ESCAPE) PostQuitMessage(0);
 		if (wParam == VK_UP) { pitch += speed; }
 		if (wParam == VK_DOWN) { pitch -= speed; }
@@ -99,9 +112,10 @@ LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam
 		if (wParam == VK_OEM_PLUS) { radius -= 0.1f; }
 		if (wParam == VK_OEM_MINUS) { radius += 0.1f; }
 		if (wParam == VK_SPACE) {
-			radius = 5.0f;              
+			radius = 10.0f;              
 			yaw = 0.0f;                 
 			pitch = 0.0f;
+			ty = tx = tx = angle = 0;
 		}
 		cameraPosition.x = target.x + radius * cos(yaw) * cos(pitch);
 		cameraPosition.y = target.y + radius * sin(pitch);
@@ -234,17 +248,164 @@ void drawSphereWithoutGLU(GLfloat radius = 0.35 ,int sliceNo = 30,int stackNo = 
 	}
 }
 
+void projection() {
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if (!ortho) {
+		gluPerspective(40, 800 / 600, Pner, Pfar);
+	}
+	else
+	{
+		glOrtho(-10, 10, -10, 10, Oner, Ofar);
+	}
+
+}
+
 void camera() {
 	gluLookAt(cameraPosition.x, cameraPosition.y, cameraPosition.z,
 		target.x, target.y, target.z,
 		upVector.x,upVector.y,upVector.z);
+	glTranslatef(tx, ty, tz);
+	glRotatef(angle, 0, 0, 1);
+}
 
+void cheast_frame1() {
+
+	glBegin(style_gl);//1
+	glColor3f(1,0,0);
+	glVertex3f(0, 0, 0);
+	glVertex3f(0,2,0.5);
+	glVertex3f(-1.6, 3.3, -1.2);
+	glVertex3f(-1.6, 2.1, -1);
+	glEnd();
+
+
+
+	glBegin(style_gl);//4
+	glColor3f(0, 1, 0);
+	glVertex3f(0, 2, 0.5);
+	glVertex3f(-1.6, 3.3, -1.2);
+	glVertex3f(-1.5, 3.4, -1.25);
+	glVertex3f(0, 2.2, 0.4);
+	glEnd();
+
+
+}
+
+void cheast_frame2() {
+	//front
+	glBegin(style_gl);
+	glColor3f(1,0,0);
+	glVertex3f(0, 0, 0);
+	glVertex3f(1.6, 0, 0);
+	glVertex3f(1.9, 0.4, 0);
+	glVertex3f(2, 1.7, 0);
+	glVertex3f(0.3, 2.6, 0);
+	glVertex3f(-0.4, 1.7, 0);
+	glEnd();
+
+	glBegin(style_gl);
+	glColor3f(0, 1, 0);
+	glVertex3f(2, 1.7, 0);
+	glVertex3f(0.3, 2.6, 0);
+	glVertex3f(-0.4, 2.8, -1.3);
+	glVertex3f(1.2, 2, -1.8);
+	glEnd();
+
+	glBegin(style_gl);
+	glColor3f(0, 0, 1);
+	glVertex3f(-0.4, 1.7, 0);
+	glVertex3f(0.3, 2.6, 0);
+	glVertex3f(-0.4, 2.8, -1.3);
+	glVertex3f(-0.8, 1.85, -1);
+	glEnd();
+
+	glBegin(style_gl);
+	glColor3f(1, 1, 0);
+	glVertex3f(1.9, 0.4, 0);
+	glVertex3f(2, 1.7, 0);
+	glVertex3f(1.2, 2, -1.8);
+	glVertex3f(1.1, 0.6, -1.8);
+	glEnd();
+
+	glBegin(style_gl);
+	glColor3f(1, 0, 1);
+	glVertex3f(1.1, 0.6, -1.8);
+	glVertex3f(1.2, -0.25, -1.4);
+	glVertex3f(1.6, 0, 0);
+	glVertex3f(1.9, 0.4, 0);
+	glEnd();
+
+	glBegin(style_gl);
+	glColor3f(1, 1, 0);
+	glVertex3f(0, 0, 0);
+	glVertex3f(1.6, 0, 0);
+	glVertex3f(1.2, -0.25, -1.4);
+	glVertex3f(-0.3, -0.2 , -1);
+	glEnd();
+
+	glBegin(style_gl);
+	glColor3f(0, 1, 0);
+	glVertex3f(-0.8, 1.85, -1);
+	glVertex3f(-0.3, -0.2, -1);
+	glVertex3f(0, 0, 0);
+	glVertex3f(-0.4, 1.7, 0);
+	glEnd();
+
+	//back
+	glBegin(style_gl);
+	glColor3f(1, 0, 0);
+	glVertex3f(-0.4, 2.8, -1.3);
+	glVertex3f(-0.8, 1.85, -1);
+	glVertex3f(-1, 1.88, -1.55);
+	glVertex3f(-0.6, 2.7, -2);
+	glEnd();
+
+	glBegin(style_gl);
+	glColor3f(0, 0, 1);
+	glVertex3f(1.2, 2, -1.8);
+	glVertex3f(-0.4, 2.8, -1.3);
+	glVertex3f(-0.6, 2.7, -2);
+	glVertex3f(0.7, 1.7, -2.9);
+	glEnd();
+
+	glBegin(style_gl);
+	glColor3f(0, 1, 1);
+	glVertex3f(-1, 1.88, -1.55);
+	glVertex3f(-0.8, 1.85, -1);
+	glVertex3f(-0.3, -0.2, -1);
+	glVertex3f(-0.85, 0.3, -2.2);
+	glVertex3f(-1.06, 1.4, -2.05);
+	glEnd();
+
+	glBegin(style_gl);
+	glColor3f(1, 0, 1);
+	glVertex3f(-1, 1.88, -1.55);
+	glVertex3f(-0.6, 2.7, -2);
+	glVertex3f(-0.3, 1.6, -2.5);
+	glVertex3f(-1.06, 1.4, -2.05);
+	glEnd();
+
+	glBegin(style_gl);
+	glColor3f(0, 1, 0);
+	glVertex3f(-0.6, 2.7, -2);
+	glVertex3f(0.7, 1.7, -2.9);
+	glVertex3f(-0.3, 1.6, -2.5);
+	glEnd();
+
+	glBegin(style_gl);
+	glColor3f(0, 1, 1);
+	glVertex3f(0.7, 1.7, -2.9);
+	glVertex3f(1.2, 2, -1.8);
+	glVertex3f(1.1, 0.6, -1.8);
+	glVertex3f(0.62, 0.55, -2.9);
+	glEnd();
 }
 
 void body_upper() {
 	glPushMatrix();
 	glTranslatef(0, -2, 0);
-	glRotatef(-90, 1, 0, 0);
+	//glRotatef(-90, 1, 0, 0);
 
 	glPushMatrix();
 	glColor3f(0, 0, 0);
@@ -262,10 +423,9 @@ void body_upper() {
 	cylinder(1.2, 0.7, 2, 20, 20, style_glu);
 	glPopMatrix();
 
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < 15; i++) {
 		glPushMatrix();
-
-		glRotatef((360 / 10) * i, 0, 0, 1);
+		glRotatef(((360 / 15) * i), 0, 0, 1);
 		glColor3f(0, 0, 0);
 		rect(1.18, 0.2, 3, style_gl);
 		glPopMatrix();
@@ -279,13 +439,20 @@ void display()
 {
 	glClearColor(0.498, 0.498, 0.498, 0);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	projection();
+
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
 	glPushMatrix();
 	camera();
 
-	body_upper();
+	//body_upper();
+	cheast_frame2();
+	//glPushMatrix();
+	//glScalef(-1, 1, 1);
+	//cheast_frame2();
+	//glPopMatrix();
 
 	glPopMatrix();//camera
 }
@@ -323,9 +490,8 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow)
 	//	make context current
 	if (!wglMakeCurrent(hdc, hglrc)) return false;
 	glEnable(GL_DEPTH_TEST);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(60, 800/600, 0.0001, 9999);
+
+	
 	//--------------------------------
 	//	End initialization
 	//--------------------------------
